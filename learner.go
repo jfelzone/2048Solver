18c8b344c347f4996c66e5b1f1376a0697d34378
Jake Felzien <jakefelzien21ati@gmail.com>
2017-11-17 18:06:13 -0700

    another update

--- learner.go
+++ learner.go
@@ -3,5 +3,19 @@ package main
 import "fmt"
 
 func main() {
+	array := [][]uint8{
+		{0, 0, 0, 0},
+		{0, 0, 0, 0},
+		{0, 0, 0, 0},
+		{0, 0, 0, 0}, 
+	}
+	for i := 0; i < 4; i++{
+		for j := 0; j < 4; j++ {
+			fmt.Print(array[i][j]);
+		}
+		fmt.Print("\n");
+	}
 	fmt.Println("Preparing to learn and dominate")
+	fmt.Println("prepare")
+	fmt.Println("hey fuck you asshole bitch titties....")
 }
